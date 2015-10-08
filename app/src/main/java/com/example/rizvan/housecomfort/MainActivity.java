8a8f742001fb6445288d0abfa2e9c7930ee14780
Rizvan <gereyhanov@gmail.com>
2015-10-08 17:44:33 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/rizvan/housecomfort/MainActivity.java
+++ app/src/main/java/com/example/rizvan/housecomfort/MainActivity.java
@@ -214,6 +214,185 @@ public class MainActivity extends AppCompatActivity {
     public static Resources getAppResources() {
         return MainActivity.mResources;
     }
+    
+    //  interface UI constructor
+    /*
+    public static boolean parseCommand (String receivedStringBLE){
+        String  intermediateDataCommand = "";
+        Integer integerCommand = 0;
 
+        if(receivedStringBLE instanceof String){
+
+            Log.i("receivedStringBLE: ", receivedStringBLE);
+
+            for (int position = 0; position < receivedStringBLE.length(); position++){
+                if('.' == receivedStringBLE.charAt(position)){
+
+                    Log.i("fullLineReceivedEnd: ", fullLineReceived);
+
+                    for (int positions = 0; positions < fullLineReceived.length(); positions++){
+                        if(':' == fullLineReceived.charAt(positions)){
+
+                            Log.i("intermediateDataCommand: ", intermediateDataCommand);
+
+                            if(intermediateDataCommand.equals("dn"))  integerCommand = 1;
+                            if(intermediateDataCommand.equals("ds"))  integerCommand = 2;
+                            if(intermediateDataCommand.equals("ok"))  integerCommand = 3;
+                            if(intermediateDataCommand.equals("err")) integerCommand = 4;
+                        }
+                        intermediateDataCommand = intermediateDataCommand + fullLineReceived.charAt(positions);
+                    }
+                }
+                fullLineReceived = fullLineReceived + receivedStringBLE.charAt(position);
+
+                Log.i("fullLineReceived: ", fullLineReceived);
+                Log.i("fullLineReceived: ", "" + fullLineReceived.length());
+            }
+
+            switch (integerCommand) {
+                case 1://dn
+                    showToastMessage("Received: dn", 1000);
+                    fullLineReceived = "";
+                    break;
+                case 2://ds
+                    createElementsdeviceUI(fullLineReceived);
+                    fullLineReceived = "";
+                    break;
+                case 3://ok
+                    flagSettingConfirmation(0x10);
+                    fullLineReceived = "";
+                    break;
+                case 4://err
+                    flagSettingConfirmation(0x20);
+                    fullLineReceived = "";
+                default:
+                    showToastMessage("Received: unknown command: " + fullLineReceived, 1000);
+                    break;
+            }
+        }else{
+            Log.e("Error parseAndActionCommand: ", "argument is not a string");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean createElementsdeviceUI (String dsStringSettingData){
+
+        StringElementsDevice stringElementsDevice = new StringElementsDevice();
+        NumberCodeDevice numberCodeDevice = new NumberCodeDevice();
+
+        stringElementsDevice.clearList();
+        numberCodeDevice.clearList();
+
+        String stNumberOfDevices;
+        String stInfoOfDevices = null;
+        String tempString = "";
+        int    counter = 0;
+        int    numberDevices = -1;
+        int    numberCodeButtons = -1;
+
+        for (int pos = 0; pos < dsStringSettingData.length(); pos++){                   
+            if(':' == dsStringSettingData.charAt(pos)){                                
+                if((pos + 1) <= dsStringSettingData.length()){
+                    stringElementsDevice.colorDevice = "" + dsStringSettingData.charAt(pos+1);
+                    stringElementsDevice.colorDevice = stringElementsDevice.colorDevice.toUpperCase();
+                }
+                if((pos + 3) <= dsStringSettingData.length()) {                         
+                    stNumberOfDevices = "" + dsStringSettingData.charAt(pos + 3);
+                    stInfoOfDevices = dsStringSettingData.substring(pos + 5, dsStringSettingData.length()); 
+                    Log.i("stInfoOfDevices", stInfoOfDevices);
+                    try {
+                        numberCodeDevice.numberOfDevices = Integer.parseInt(stNumberOfDevices, 10); 
+                    }catch (NumberFormatException err){
+                        Log.i("Parse Int falied", "" + err.getMessage());
+                    }
+                }
+            }
+        } // out stInfoOfDevices, numberOfDevices
+
+        // string date stInfoOfDevices
+        // TITLE1,_____00_________0_________ON/OFF,____1________31______ON/OFF.
+        //   |         |          |           |
+        // nameDEV  codeBut    numDev        name
+
+        Log.i("switch for date: --> ",  stInfoOfDevices.toString());
+
+        for(int pos = 0; pos < stInfoOfDevices.length(); pos++){
+            if((',' == stInfoOfDevices.charAt(pos)) | ('.' == stInfoOfDevices.charAt(pos))){
+                switch (counter){
+                    case 0:// Title Device
+                        stringElementsDevice.titleDevice = tempString;
+                        tempString = "";
+                        break;
+                    case 1:
+                        try {
+                            numberCodeButtons = Integer.parseInt(tempString.substring(0, 2), 10);
+                            numberDevices = Integer.parseInt("" + (tempString.charAt(2)), 10);
+                        }catch (NumberFormatException err){
+                            Log.i("Parse Int falied ", "case 1:" + err.getMessage());
+                        }
+                        numberCodeDevice.numberDevices = numberDevices;
+                        numberCodeDevice.numberCodeButtons_1 = numberCodeButtons;
+                        stringElementsDevice.titleOptions_1 = tempString.substring(3, tempString.length());
+                        tempString = "";
+                        break;
+                    case 2:
+                        try {
+                            numberCodeButtons = Integer.parseInt(tempString.substring(0, 2), 10);
+                            numberDevices = Integer.parseInt("" + (tempString.charAt(2)), 10);
+                        }catch (NumberFormatException err){
+                            Log.i("Parse Int falied ", "case 2:" + err.getMessage());
+                        }
+                        numberCodeDevice.numberCodeButtons_2 = numberCodeButtons;
+                        stringElementsDevice.titleOptions_2 = tempString.substring(3, tempString.length());
+                        tempString = "";
+                        break;
+                    case 3:
+                        try {
+                            numberCodeButtons = Integer.parseInt(tempString.substring(0, 2), 10);
+                            numberDevices = Integer.parseInt("" + (tempString.charAt(2)), 10);
+                        }catch (NumberFormatException err){
+                            Log.i("Parse Int falied ", "case 3:" + err.getMessage());
+                        }
+                        numberCodeDevice.numberCodeButtons_3 = numberCodeButtons;
+                        stringElementsDevice.titleOptions_3 = tempString.substring(3, tempString.length());
+                        tempString = "";
+                        break;
+                    case 4:
+                        try {
+                            numberCodeButtons = Integer.parseInt(tempString.substring(0, 2), 10);
+                            numberDevices = Integer.parseInt("" + (tempString.charAt(2)), 10);
+                        }catch (NumberFormatException err){
+                            Log.i("Parse Int falied ", "case 4:" + err.getMessage());
+                        }
+                        numberCodeDevice.numberCodeButtons_4 = numberCodeButtons;
+                        stringElementsDevice.titleOptions_4 = tempString.substring(3, tempString.length());
+                        tempString = "";
+                        break;
+                    case 5:
+                        try {
+                            numberCodeButtons = Integer.parseInt(tempString.substring(0, 2), 10);
+                            numberDevices = Integer.parseInt("" + (tempString.charAt(2)), 10);
+                        }catch (  NumberFormatException err){
+                            Log.i("Parse Int falied ", "case 5:" + err.getMessage());
+                        }
+                        numberCodeDevice.numberCodeButtons_5 = numberCodeButtons;
+                        stringElementsDevice.titleOptions_5 = tempString.substring(3, tempString.length());
+                        tempString = "";
+                        break;
+                }
+                counter++;
+            } else tempString = tempString + stInfoOfDevices.charAt(pos);
+
+        }
+
+        FragmentActivityDevice.remakingUI(stringElementsDevice, numberCodeDevice);
+        return true;
+    }
+
+    public static void flagSettingConfirmation(int flag){
+        showToastMessage("Received: unknown number" + flag, 1000);
+    }
+    */
 
 }
